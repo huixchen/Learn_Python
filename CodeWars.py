d87e715eac9483824e09592cff44041d59e391a6
cuichenli <858450700@qq.com>
2017-06-09 09:52:23 +1000

    Create CodeWars.py

--- CodeWars.py
+++ CodeWars.py
@@ -92,3 +92,27 @@ def largest(string):
     return max(result)
 
 print(largest(299999999))
+
+
+###########################################
+#Challenge 3 Unique Code
+###########################################
+
+# Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+# 
+# For example:
+# 
+# unique_in_order('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+# unique_in_order('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+# unique_in_order([1,2,2,3,3])       == [1,2,3]
+def unique_in_order(iterable):
+    pre_item = None
+    result = []
+    for item in iterable:
+        if item != pre_item:
+            result.append(item)
+            pre_item = item  #important!!!
+    return result
+
+
+
