143d2935f5c940602aad519e07a6cebb9a413804
cuichenli <858450700@qq.com>
2017-06-16 23:34:10 +1000

    Update CodeWars.py

--- CodeWars.py
+++ CodeWars.py
@@ -346,3 +346,34 @@ def parse_html_color(color):
         return {'r': int(color[1:3], 16), 'g': int(color[3:5], 16), 'b': int(color[5:7], 16)}
     else:
         return {'r': int(color[1]*2, 16), 'g': int(color[2]*2, 16), 'b': int(color[3]*2, 16)}
+    
+    
+  ##########################################
+# Challenge 10 Tic-Tac-Toe Checker
+##########################################
+# If we were to set up a Tic-Tac-Toe game, we would want to know whether the board's current state is solved, wouldn't we? Our goal is to create a function that will check that for us!
+# 
+# Assume that the board comes in the form of a 3x3 array, where the value is 0 if a spot is empty, 1 if it is an X, or 2 if it is an O, like so:
+# 
+# [[0,0,1],
+#  [0,1,2],
+#  [2,1,0]]
+# We want our function to return -1 if the board is not solved yet, 1 if X won, 2 if O won, or 0 if it's a cat's game (i.e. a draw).
+# 
+# You may assume that the board passed in is valid in the context of a game of Tic-Tac-Toe.
+def isSolved(board):
+  for i in range(0,3):
+    if board[i][0] == board[i][1] == board[i][2] != 0:
+      return board[i][0]
+    elif board[0][i] == board[1][i] == board[2][i] != 0:
+      return board[0][i]
+      
+  if board[0][0] == board[1][1] == board[2][2] != 0:
+    return board[0][0]
+  elif board[0][2] == board[1][1] == board[2][0] != 0:
+    return board[0][0]
+
+  elif 0 not in board[0] and 0 not in board[1] and 0 not in board[2]:
+    return 0
+  else:
+    return -1
